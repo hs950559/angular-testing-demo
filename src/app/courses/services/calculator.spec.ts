370fa04d1b4fcabc8a3d20e7486937ad31173181
Hemant Kumar Singh <hemant@Hemants-MacBook-Air.local>
2020-05-13 10:17:27 +0530

    add/subtract test added

--- src/app/courses/services/calculator.spec.ts
+++ src/app/courses/services/calculator.spec.ts
@@ -1,9 +1,26 @@
+import { CalculatorService } from './calculator.service';
+import { LoggerService } from './logger.service';
+
 describe('CalculatorService', () => {
   it('should add 2 numbers', () => {
-    pending();
+    // Step 1 - setup
+    const calculator = new CalculatorService(new LoggerService());
+
+    // Step 2 - Act
+    const result = calculator.add(2, 4);
+
+    // Step 3 - Assert
+    expect(result).toEqual(6);
   });
 
   it('should subtract 2 numbers', () => {
-    fail();
+    // Step 1 - setup
+    const calculator = new CalculatorService(new LoggerService());
+
+    // Step 2 - Act
+    const result = calculator.subtract(10, 5);
+
+    // Step 3 - Assert
+    expect(result).toEqual(5);
   });
 });

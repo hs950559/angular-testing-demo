681bf360a3e126f3cd66449bcf99cbbe965a61f6
Hemant Kumar Singh <hemant@Hemants-MacBook-Air.local>
2020-05-13 10:43:11 +0530

    faking logger service

--- src/app/courses/services/calculator.spec.ts
+++ src/app/courses/services/calculator.spec.ts
@@ -1,26 +1,20 @@
 import { CalculatorService } from './calculator.service';
-import { LoggerService } from './logger.service';
 
 describe('CalculatorService', () => {
   it('should add 2 numbers', () => {
     // Step 1 - setup
-    const calculator = new CalculatorService(new LoggerService());
+    const logger = jasmine.createSpyObj('LoggerService', ['log']);
+    const calculator = new CalculatorService(logger);
 
     // Step 2 - Act
     const result = calculator.add(2, 4);
 
     // Step 3 - Assert
     expect(result).toEqual(6);
+    expect(logger.log).toHaveBeenCalledTimes(1);
   });
 
   it('should subtract 2 numbers', () => {
-    // Step 1 - setup
-    const calculator = new CalculatorService(new LoggerService());
-
-    // Step 2 - Act
-    const result = calculator.subtract(10, 5);
-
-    // Step 3 - Assert
-    expect(result).toEqual(5);
+    pending();
   });
 });

2f3f6f19fa8c6d06f5625b21c5a330154914b41e
Hemant Kumar Singh <hemant@Hemants-MacBook-Air.local>
2020-05-13 21:36:28 +0530

    should retreive all courses

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,23 +1,39 @@
-import { HttpTestingController } from '@angular/common/http/testing';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { CoursesService } from './courses.service';
+import { COURSES } from '../../../../server/db-data';
 
 describe('CoursesService', () => {
   let coursesService: CoursesService;
+  let testingController: HttpTestingController;
 
   beforeEach(() => {
 
     TestBed.configureTestingModule({
-      imports: [HttpTestingController],
+      imports: [HttpClientTestingModule],
       providers: [
         CoursesService
       ]
     });
 
     coursesService = TestBed.get(CoursesService);
+    testingController = TestBed.get(HttpTestingController);
   });
 
   it('should retreive all courses', () => {
+    coursesService.findAllCourses()
+      .subscribe(courses => {
+        expect(courses.length).toBe(12);
 
+        const course = courses[1];
+        expect(course.category).toEqual('BEGINNER');
+        expect(course.titles.description).toEqual('Angular Core Deep Dive');
+
+
+      });
+
+    const req = testingController.expectOne('/api/courses');
+    expect(req.request.method).toEqual('GET');
+    req.flush({ payload: Object.values(COURSES) });
   });
 });

55223e8d69f7b17265c66afb61186cfa1309d9f3
Hemant Kumar Singh <hemant@Hemants-MacBook-Air.local>
2020-05-13 15:49:20 +0530

    subtract test - faking logger service

--- src/app/courses/services/calculator.spec.ts
+++ src/app/courses/services/calculator.spec.ts
@@ -15,6 +15,15 @@ describe('CalculatorService', () => {
   });
 
   it('should subtract 2 numbers', () => {
-    pending();
+    // Step 1 - setup
+    const logger = jasmine.createSpyObj('LoggerService', ['log']);
+    const calculator = new CalculatorService(logger);
+
+    // Step 2 - Act
+    const result = calculator.subtract(12, 4);
+
+    // Step 3 - Assert
+    expect(result).toEqual(8);
+    expect(logger.log).toHaveBeenCalledTimes(1);
   });
 });

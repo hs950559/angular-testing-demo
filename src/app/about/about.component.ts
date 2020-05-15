29ab97b93daf8a8be1195ca7771340332c7bd42d
Hemant Kumar Singh <hemant@Hemants-MacBook-Air.local>
2020-05-15 10:07:51 +0530

    lint fix

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'about',
+  selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
